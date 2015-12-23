46c32b409b118bb464d618aca31aaaf5954be5e2
jeroenvinken <jeroen_vinken@hotmail.com>
2015-12-23 19:26:11 +0100

    time for the no error run

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -12,33 +12,14 @@ class Index extends CI_Controller {
     }
 
     public function index() {
-        $data['title'] = 'Buromas - Bureaustoelen';
+        $data['title'] = 'Frietonline - home';
         $data['pagina'] = 'index';
         
-        $this->load->model('serie_model');
+        /*$this->load->model('serie_model');
         $series = $this->serie_model->getAllWithNoHoofdrubriek();
-        $data['series'] = $series;
+        $data['series'] = $series;*/
 
         $partials = array('header' => 'main_header', 'content' => 'index', 'footer' => 'main_footer');
         $this->template->load('main_master', $partials, $data);
-    }
-    
-    public function overburomas() {
-        $data['title'] = 'Buromas - Bureaustoelen';
-        $data['pagina'] = 'index';       
-        
-
-        $partials = array('header' => 'main_header', 'content' => 'overburomas', 'footer' => 'main_footer');
-        $this->template->load('main_master', $partials, $data);
-    }
-     
-    public function series() {
-        $data['title'] = 'Buromas - Bureaustoelen';
-        $data['pagina'] = 'index';       
-        
-
-        $partials = array('header' => 'main_header', 'content' => 'series', 'footer' => 'main_footer');
-        $this->template->load('main_master', $partials, $data);
-    }
-
+    } 
 }
